8b49c2d5ddc8f030676f837d3c16d8fefe50bafd
Britt Davison <britt.davison@outlook.com>
2022-02-25 16:50:34 -0600

    add products router and repository

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const express = require('express');
 const cookieSession = require('cookie-session');
 // const { response } = require('express');
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({
     keys: ['asdfas$#df#$%#$%asdfasfasdf']
 }));
 app.use(authRouter);
+app.use(productsRouter);
 
 
 
