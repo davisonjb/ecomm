fea687b374daeb5b27a13336dadfdb90db3e8ced
Britt Davison <britt.davison@outlook.com>
2022-02-25 08:11:56 -0600

    complete users class

--- repositories/users.js
+++ repositories/users.js
@@ -43,14 +43,41 @@ class UsersRepository {
         return records.find(record => record.id === id);
 
     }
+
+    async delete(id) {
+        const records = await this.getAll();
+        const filteredRecords = records.filter(record => record.id !== id);
+        await this.writeAll(filteredRecords);
+    }
+
+    async update(id, attrs) {
+        const records = await this.getAll();
+        const record = records.find(record => record.id === id);
+        if (!record) {
+            throw new Error(`Record with id ${id} not found.`);
+        }
+        console.log(records.indexOf(record));
+        Object.assign(record, attrs);
+        await this.writeAll(records);
+    }
+
+    async getOneBy(filters) {
+        const records = await this.getAll();
+        for (let record of records) {
+            let found = true;
+
+            for (let key in filters) {
+                if (record[key] !== filters[key]) {
+                    found = false;
+                }
+            }
+            if (found) {
+                return record;
+
+            }
+        }
+
+    }
 };
 
-const test = async() => {
-    const repo = new UsersRepository('users.json');
-
-    const user = await repo.getOne('e27beade');
-
-    console.log(user);
-}
-
-test();
+module.exports = new UsersRepository('users.json');
